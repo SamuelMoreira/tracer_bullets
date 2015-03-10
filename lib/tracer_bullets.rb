ad067a5f5429bc114fd19d1e809c9fe8267882d3
Samuel <genesis.samuel@gmail.com>
2015-03-10 12:38:11 +0000

    remove environment condition

--- lib/tracer_bullets.rb
+++ lib/tracer_bullets.rb
@@ -4,10 +4,8 @@ module TracerBullets
 
   module Methods
     def tracer_bullet
-      if Rails.env.development?
-        _tracer_bullets_log( "Elapsed: #{((Time.now - @tracer_bullet_start_time)*1000).to_i}ms #{caller(0)[1]}" )
-        @tracer_bullet_start_time = Time.now
-      end
+      _tracer_bullets_log( "Elapsed: #{((Time.now - @tracer_bullet_start_time)*1000).to_i}ms #{caller(0)[1]}" )
+      @tracer_bullet_start_time = Time.now
     end
     alias_method :tb, :tracer_bullet
 
